0f4b6cd3611cf32e0daa54d6b3d5d3c98aa5bee2
biniam <biniamdegu55@gmail.com>
2024-03-31 10:34:10 -0900

    google login dependency conflict resolved

--- src/components/FrameComponent4.js
+++ src/components/FrameComponent4.js
@@ -1,16 +1,16 @@
 import "./FrameComponent4.css";
-import {
-  GoogleLogin,
-  GoogleLoginResponse,
-  GoogleLoginResponseOffline,
-} from "react-google-login";
+// import {
+//   GoogleLogin,
+//   GoogleLoginResponse,
+//   GoogleLoginResponseOffline,
+// } from "react-google-login";
 const FrameComponent4 = () => {
-  const responseGoogle = (response) => {
-    console.log(response);
-  };
-  const responseGoogleError = (error) => {
-    console.error(error);
-  };
+  // const responseGoogle = (response) => {
+  //   console.log(response);
+  // };
+  // const responseGoogleError = (error) => {
+  //   console.error(error);
+  // };
   return (
     <div className="button-parent">
       <button className="button3">
@@ -18,14 +18,14 @@ const FrameComponent4 = () => {
       </button>
       <div className="#">
         {" "}
-        <GoogleLogin
+        {/* <GoogleLogin
           clientId="YOUR_GOOGLE_CLIENT_ID"
           buttonText="Continue with Google"
           onSuccess={responseGoogle}
           onFailure={responseGoogleError}
           cookiePolicy={"single_host_origin"}
           className={"button4"}
-        />
+        /> */}
       </div>
 
       <div className="already-have-an-account-parent">
